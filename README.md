84bd8b317e2540ad405613ce267dae19cf39f032
Jim Bacon <boftx@hotmail.com>
2022-04-16 13:33:22 -0700

    Initial module structure cmmit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Calendar-Fiscal5253
-Create 4-5-4 and 4-4-5 fiscal calendars
---
+++ lib/Calendar/Fiscal5253.pm
@@ -0,0 +1,109 @@
+package Calendar::Fiscal5253;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Calendar::Fiscal5253 - The great new Calendar::Fiscal5253!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Calendar::Fiscal5253;
+
+    my $foo = Calendar::Fiscal5253->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jim Bacon, C<< <boftx at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-calendar-fiscal5253 at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Calendar-Fiscal5253>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Calendar::Fiscal5253
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Calendar-Fiscal5253>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Calendar-Fiscal5253>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Calendar-Fiscal5253>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Calendar-Fiscal5253>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Jim Bacon.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Calendar::Fiscal5253
